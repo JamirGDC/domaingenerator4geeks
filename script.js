d88600e86c9345448f2988a203003f0c3979f09d
JamirGDC <gutierrez.dc@icloud.com>
2023-09-24 01:06:41 +0200

    correccion

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const parrafo = document.getElementById("domain");
-const onload = document.getElementById("onload");
-
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
 
-// let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-// let action = ['ate', 'peed', 'crushed', 'broke'];
-// let what = ['my homework', 'the keys', 'the car'];
-// let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
-
 function generatedomain() {
-    parrafo.innerHTML = pronoun[Math.floor(Math.random() * pronoun.length)] + adj[Math.floor(Math.random() * adj.length)] + noun[Math.floor(Math.random() * noun.length)] + ".com";
+    for (let i = 0; i < pronoun.length; i++) {
+        for (let j = 0; j < adj.length; j++) {
+            for (let k = 0; k < noun.length; k++) {
+                console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+            }
+        }
+    }
 }
 
-addEventListener("load", generatedomain);
-onload.addEventListener("click", generatedomain);
+generatedomain();
